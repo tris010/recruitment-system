18785feb5094a4dad7eae902339558fe1b1d2523
Avani <122514712+its-a-mystery-ship-it@users.noreply.github.com>
2026-02-11 17:51:58 +0530

    debug: test db creation

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import sqlalchemy
+from sqlalchemy.orm import Session
+from db import Base, engine, get_db
 
 app = FastAPI()
 
+# Create tables
+try:
+    Base.metadata.create_all(bind=engine)
+    TABLES_CREATED = True
+except Exception as e:
+    TABLES_CREATED = str(e)
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World", "SA": sqlalchemy.__version__}
+    return {"Hello": "World", "SA": sqlalchemy.__version__, "Tables": TABLES_CREATED}
 
 @app.get("/health")
 def health():
